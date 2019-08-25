a5e71f39eaeaac507a88e8e2349a730d966af599
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-08-25 13:42:59 +0900

    edit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,30 @@
-CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
+if Rails.env.production?
+  CarrierWave.configure do |config|
 
-  case Rails.env
-    when 'production'
-     config.fog_credentials = {
-     provider:                'AWS',
-     aws_access_key_id: "your-aws-id",
-     aws_secret_access_key: "your-secret-key",
-     region:                  'your-region'
+    case Rails.env
+      when 'production'
+      config.fog_credentials = {
+      provider:                'AWS',
+      aws_access_key_id: "your-aws-id",
+      aws_secret_access_key: "your-secret-key",
+      region:                  'your-region'
+      }
+        config.fog_directory = 'your-bucket-name'
+        config.asset_host = 'your-bucket-url'
+
+      when 'staging'
+      config.fog_credentials = {
+      provider:                'AWS',
+      aws_access_key_id:       "your-secret-id",
+      aws_secret_access_key:   "your-secret-key",
+      region:                  'your-region'
     }
-      config.fog_directory = 'your-bucket-name'
-      config.asset_host = 'your-bucket-url'
-
-    when 'staging'
-     config.fog_credentials = {
-     provider:                'AWS',
-     aws_access_key_id:       "your-secret-id",
-     aws_secret_access_key:   "your-secret-key",
-     region:                  'your-region'
-   }
-      config.fog_directory = 'your-secret-bucket-name'
-      config.asset_host = 'your-buucket-url'
+        config.fog_directory = 'your-secret-bucket-name'
+        config.asset_host = 'your-buucket-url'
+    end
   end
 end

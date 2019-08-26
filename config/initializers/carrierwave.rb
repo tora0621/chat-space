db8968e194371faf8019767a5965dd761ed6149b
平野孝太朗 <hiranokotaro@hiranokoutaakiranoMacBook-Air.local>
2019-08-26 12:59:30 +0900

    secrets

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,30 +1,43 @@
 require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
+require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
+# if Rails.env.production?
+#   CarrierWave.configure do |config|
 
-    case Rails.env
-      when 'production'
-      config.fog_credentials = {
-      provider:                'AWS',
-      aws_access_key_id: "your-aws-id",
-      aws_secret_access_key: "your-secret-key",
-      region:                  'your-region'
-      }
-        config.fog_directory = 'your-bucket-name'
-        config.asset_host = 'your-bucket-url'
+#     case Rails.env
+#       when 'production'
+#       config.fog_credentials = {
+#       provider:                'AWS',
+#       aws_access_key_id: "your-aws-id",
+#       aws_secret_access_key: "your-secret-key",
+#       region:                  'your-region'
+#       }
+#         config.fog_directory = 'your-bucket-name'
+#         config.asset_host = 'your-bucket-url'
 
-      when 'staging'
-      config.fog_credentials = {
-      provider:                'AWS',
-      aws_access_key_id:       "your-secret-id",
-      aws_secret_access_key:   "your-secret-key",
-      region:                  'your-region'
-    }
-        config.fog_directory = 'your-secret-bucket-name'
-        config.asset_host = 'your-buucket-url'
-    end
-  end
+#       when 'staging'
+#       config.fog_credentials = {
+#       provider:                'AWS',
+#       aws_access_key_id:       "your-secret-id",
+#       aws_secret_access_key:   "your-secret-key",
+#       region:                  'your-region'
+#     }
+#         config.fog_directory = 'your-secret-bucket-name'
+#         config.asset_host = 'your-buucket-url'
+#     end
+#   end
+# end
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1' 
+  }
+
+  config.fog_directory  = 'toratora0621'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/toratora0621'
 end
